48a06ce8772def9f20c4de079b492693aa4c51b4
Cristian <1154@holbertonschool.com>
2020-05-16 18:03:59 +0000

    Adding file 2hbnb.py 2hbnb.js and modified app.py of api folder and add class in 3-headers css file of task 3

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -43,4 +43,4 @@ def hbnb():
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,31 @@
+/* script allows to listen for changes on each INPUT checkbox tag */
+
+$(() => {
+  $.get('http://0.0.0.0:5001/api/v1/status/', (response) => {
+    if (response.status === 'OK') {
+      $('header #api_status').addClass('available');
+    }
+  });
+  const dirAmeny = {};
+  $('li input:checkbox').css({ 'margin-right': '10px' });
+  $('li input:checkbox').change(function () {
+    if ($(this).prop('checked')) {
+      // $('li input:checkbox').css('background-color','#33A8FF');
+      dirAmeny[$(this).attr('data-id')] = $(this).attr('data-name');
+      let allValues = Object.values(dirAmeny);
+      allValues = allValues.join(', ');
+      $('.amenities h4 p').html(allValues);
+    } else {
+      // $('li input:checkbox').css('background-color', '#FDFEFE');
+      const idAmenyRemov = $(this).attr('data-id');
+      delete dirAmeny[idAmenyRemov];
+      let updateValues = Object.values(dirAmeny);
+      updateValues = updateValues.join(', ');
+      if (updateValues.length === 0) {
+        $('.amenities h4 p').html('&nbsp;');
+      } else {
+        $('.amenities h4 p').html(updateValues);
+      }
+    }
+  });
+});
