cc0854518b120c031fa9ba5fa91a2f417ce098a5
mlgomez0 <mlgomez230@hotmail.com>
2020-05-16 15:13:40 +0000

    Adding tag p into amenities h4

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,36 +1,26 @@
 /* script allows to listen for changes on each INPUT checkbox tag */
 
 $(() => {
-  let dirAmeny = {};
-  let str_ameny = null;
-  $('li input:checkbox').css({'margin-right':'10px'});
+  const dirAmeny = {};
+  $('li input:checkbox').css({ 'margin-right': '10px' });
   $('li input:checkbox').change(function () {
     if ($(this).prop('checked')) {
-      //$('li input:checkbox').css('background-color','#33A8FF');
+      // $('li input:checkbox').css('background-color','#33A8FF');
       dirAmeny[$(this).attr('data-id')] = $(this).attr('data-name');
       let allValues = Object.values(dirAmeny);
       allValues = allValues.join(', ');
-      $('.amenities h4').text(allValues);
-
-      /*
-      if(str_ameny === null) {
-        strAmeny = dirAmeny[$(this).attr('data-id')]
-      } else {
-        strAmeny = strAmeny + "," + dirAmeny[$(this).attr('data-id')];
-      }
-      $('.amenities h4').text(strAmeny);*/
-      console.log(dirAmeny);
-
+      $('.amenities h4 p').html(allValues);
     } else {
-      //$('li input:checkbox').css('background-color', '#FDFEFE');
+      // $('li input:checkbox').css('background-color', '#FDFEFE');
       const idAmenyRemov = $(this).attr('data-id');
-      console.log(idAmenyRemov);
       delete dirAmeny[idAmenyRemov];
-      //$('.amenities h4').empty();
       let updateValues = Object.values(dirAmeny);
       updateValues = updateValues.join(', ');
-      $('.amenities h4').text(updateValues);
-      console.log(dirAmeny);
+      if (updateValues.length === 0) {
+        $('.amenities h4 p').html('&nbsp;');
+      } else {
+        $('.amenities h4 p').html(updateValues);
+      }
     }
   });
 });
